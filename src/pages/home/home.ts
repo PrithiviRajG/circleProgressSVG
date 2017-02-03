08b93d0e492b6edbccd5e28623767f33a004b9b1
Prithivi Raj G <prithiviraj1991@gmail.com>
2017-02-03 20:07:39 +0530

    changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,23 +10,31 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+
+  }
+
+  ionViewDidLoad(){
     let  val:any = parseInt("50");
- //var $circle = document.getElementById("bar").getAttribute('r');
+ let $circle = document.querySelector('#svg #bar');
  if (isNaN(val)) {
   val = 100;
  }
  else{
-   let r:any = document.getElementById("bar").getAttribute('r');
+   let r: any = document.querySelector('#svg #bar').getAttribute('r').valueOf();
+   //let r=90
    var c = Math.PI*(r*2);
 
    if (val < 0) { val = 0;}
    if (val > 100) { val = 100;}
 
    var pct = ((100-val)/100)*c;
+   console.log(pct)
 
    //$circle.css({ strokeDashoffset: pct});
-
+   document.getElementById("bar").style.strokeDashoffset =pct.toString();
    document.getElementById("cont").setAttribute("data-pct",val)
+
+   //document.getElementById("cont").setAttribute("data-pct",val)
  }
 
   }
